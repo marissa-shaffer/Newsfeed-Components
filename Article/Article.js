d211ac8cf835849020a196b2e55068de47a2eed4
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-02-20 18:49:30 -0600

    Added create Article Function with 5 argments

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,11 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+let createArticle = (headerContent, dateContent, para1Content, para2Content, para3Content) => {
+
+  let article = document.createElement('div');
+  article.classList.add('article');
+
+  
+}
