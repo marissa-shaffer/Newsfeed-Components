d62c29d63821c75c8abe12fc19aab05de7261be8
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-02-20 20:18:04 -0600

    Article note edit. imported menu into index less.

--- Article/Article.js
+++ Article/Article.js
@@ -112,7 +112,6 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
-// create a .forEach for data
 
 
 let createArticle = (headerContent, dateContent, para1Content, para2Content, para3Content) => {
